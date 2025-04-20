b1d1a97e8092db5334e29767ad45a665b1ae65ba
Bharat Bhushan <bharat@Bharats-MacBook-Pro.local>
2025-04-20 15:37:01 +0100

    Initialise ecocrock project- first commit

---
+++ src/main/java/com/ecocrock/EcoCrock/EcoCrockApplication.java
@@ -0,0 +1,13 @@
+package com.ecocrock.EcoCrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoCrockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecocrock/EcoCrock/EcoCrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocrock.EcoCrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoCrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
